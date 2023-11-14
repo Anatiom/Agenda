93fb6c67c97571c0a03e933a83362dde76977df3
Anatiom Martins <henrique.anderson6@gmail.com>
2023-11-14 17:21:02 -0300

    Create

--- config/process.php
+++ config/process.php
@@ -3,21 +3,47 @@ session_start();
 include_once('connection.php');
 include_once('url.php');
 
-// Retorna um único contato 
-$id;
-if(!empty($_GET)){
-    $id = $_GET['id'];
-}
-if(!empty($id)){ 
-    $query = "SELECT * FROM contacts WHERE id=:id";
-    $stmp = $conn->prepare($query);
-    $stmp->bindParam(":id",$id);
-    $stmp->execute();
-    $contact = $stmp->fetch();
+
+$data = $_POST;
+if(!empty($data)){
+    if($data['type'] === 'create'){
+        $name = $data['name'];
+        $phone = $data['phone'];
+        $observations = $data['observations'];
+        $query = "INSERT INTO contacts (name, phone, observations) 
+        VALUES(:name, :phone, :observations)";
+        $stmp = $conn->prepare($query);
+        $stmp->bindParam(":name", $name);
+        $stmp->bindParam(":phone", $phone);
+        $stmp->bindParam(":observations", $observations);
+        try{
+            $stmp->execute();
+            $_SESSION['msg'] = 'Contato criado com sucesso!';
+          
+        }catch(PDOException $e){
+            $error = $e->getMessage();
+            echo "Error: $error";
+        }
+        header("Location:". $BASE_USL."../index.php");
+    }
 }else{
-    // Retorna todo os contatos 
-    $query = "SELECT * FROM contacts";
-    $stmp = $conn->prepare($query);
-    $stmp->execute();
-    $contacts = $stmp->fetchAll();
+    // Retorna um único contato 
+    $id;
+    if(!empty($_GET)){
+        $id = $_GET['id'];
+    }
+    if(!empty($id)){ 
+        $query = "SELECT * FROM contacts WHERE id=:id";
+        $stmp = $conn->prepare($query);
+        $stmp->bindParam(":id",$id);
+        $stmp->execute();
+        $contact = $stmp->fetch();
+    }else{
+        // Retorna todo os contatos 
+        $query = "SELECT * FROM contacts";
+        $stmp = $conn->prepare($query);
+        $stmp->execute();
+        $contacts = $stmp->fetchAll();
+    }
 }
+$conn = null;
--- create.php
+++ create.php
@@ -1,3 +1,24 @@
 <?php include_once('./templates/header.php') ?>
-    <h1>Create</h1>
+    <div class="container">
+    <?php require_once('templates/backbtn.html'); ?>
+    <h1 id="main-title-show">
+        Criar contato
+    </h1>
+    <form id="create-form" action="<?php $BASE_URL; ?>config/process.php" method="POST">
+        <input type="hidden" name="type" value="create">
+        <div class="from-group">
+            <label for="name">Nome do contato:</label>
+            <input type="text" name="name" id="name" required class="form-control" placeholder="Digite o nome" >
+        </div>
+        <div class="from-group">
+            <label for="phone">Telefone do contato:</label>
+            <input type="text" name="phone" id="phone" required class="form-control" placeholder="(xx)xxxx-xxxx" >
+        </div>
+        <div class="from-group">
+            <label for="observations">Observações:</label>
+            <textarea name="observations" id="observations" class="form-control" rows="3" placeholder="Sua observações"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Cadastrar</button>
+    </form>
+    </div>
 <?php include_once('./templates/footer.php') ?>
